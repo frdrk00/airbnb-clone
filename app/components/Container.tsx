4b76cb2bfc79391d36575b3b0c6a5977d9552451
frdrk00 <masterkooo@gmail.com>
2023-04-01 20:23:19 +0200

    Building: Navbar

---
+++ app/components/Container.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+interface ContainerProps {
+    children: React.ReactNode
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div className="
+        max-w-[2520px] 
+        mx-auto 
+        xl:px20 
+        md:px10 
+        sm:px-2 
+        px-4"
+    >
+        {children}
+    </div>
+  )
+}
+
+export default Container
